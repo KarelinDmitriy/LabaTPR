552688dd51c89311c2aeff94509a81ab833d5d6f
Dmitriy Karelin <bykarelin@gmail.com>
2014-04-07 14:16:20 +0400

    Add Matrix.cs

---
+++ SimplexModel/Matrix.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimplexModel
+{
+    class Matrix
+    {
+#region variable
+        int _n, _m;
+        double[,] _matrix;
+#endregion 
+
+#region public methods
+        public Matrix(int n, int m)
+        {
+            _matrix = new double[n, m];
+            _n = n;
+            _m = m;
+        }
+
+        public double this[int i, int j]
+        {
+            get
+            {
+                return _matrix[i, j];
+            }
+            set
+            {
+                _matrix[i, j] = value;
+            }
+        }
+#endregion
+
+#region private methods
+
+#endregion
+    }
+}
